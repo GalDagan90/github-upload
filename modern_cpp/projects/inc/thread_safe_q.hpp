eb7c1d013d389709033469261303b428db88a91d
GalDagan90 <gdag90@gmail.com>
2024-02-15 21:08:30 +0200

    thread_safe_q first work

--- modern_cpp/projects/inc/thread_safe_q.hpp
+++ modern_cpp/projects/inc/thread_safe_q.hpp
@@ -1,6 +1,7 @@
 #ifndef __GAL_DAGAN_PROJ_THREAD_SAFE_Q__
 #define __GAL_DAGAN_PROJ_THREAD_SAFE_Q__
 
+#include <iostream>
 #include <queue>
 #include <mutex>
 #include <condition_variable>
@@ -14,18 +15,22 @@ class ThreadSafeQueue
 private:
     Q m_queue;
     mutable std::mutex m_mutex;
+    std::condition_variable m_cond_var;
 
 public:
     ThreadSafeQueue() = default;
     ~ThreadSafeQueue() = default;
-    ThreadSafeQueue(const ThreadSafeQueue& other);
-    ThreadSafeQueue& operator=(const ThreadSafeQueue& other);
+    ThreadSafeQueue(const ThreadSafeQueue& other) = delete;
+    ThreadSafeQueue& operator=(const ThreadSafeQueue& other) = delete;
+
+    ThreadSafeQueue(ThreadSafeQueue&& other) = delete;
+    ThreadSafeQueue& operator=(ThreadSafeQueue&& other) = delete;
 
     bool empty() const;
     std::size_t size() const;
     
     void push(T val);
-    
+
     void wait_and_pop(T& res);
     std::shared_ptr<T> wait_and_pop();
     
@@ -36,8 +41,31 @@ public:
     std::shared_ptr<T> try_pop();
 };
 
+/********************************************************************************/
+/*                        Implementation Detail                                 */
+/********************************************************************************/
 
+template <typename T, typename Q>
+bool ThreadSafeQueue<T, Q>::empty() const
+{
+    std::lock_guard<std::mutex> lk(m_mutex);
+    return m_queue.empty();
+}
 
+template <typename T, typename Q>
+std::size_t ThreadSafeQueue<T, Q>::size() const
+{
+    std::lock_guard<std::mutex> lk(m_mutex);
+    return m_queue.size();
+}
+
+template <typename T, typename Q>
+void ThreadSafeQueue<T, Q>::push(T val)
+{
+    std::lock_guard<std::mutex> lk(m_mutex);
+    m_queue.push(std::move(val));
+    m_cond_var.notify_one();
+}
 
 
 #endif //"__GAL_DAGAN_PROJ_THREAD_SAFE_Q__"
--- modern_cpp/projects/test/thread_safe_q_test.cpp
+++ modern_cpp/projects/test/thread_safe_q_test.cpp
@@ -1,7 +1,19 @@
 #include "thread_safe_q.hpp"
 
+#include <iostream>
+
+class Task
+{
+    Task();
+    void operator()(int x)
+    {
+        using namespace std;
+        cout << "printed: " << x << "\n";
+    }
+};
+
 int main()
 {
-
+    ThreadSafeQueue<Task> q;
     return 0;
 }
