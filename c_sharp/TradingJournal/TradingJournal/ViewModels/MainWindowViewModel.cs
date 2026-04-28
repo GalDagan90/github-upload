fa48da262118fe3f868736e0d685d25ef54e5de5
GalDagan90 <gdag90@gmail.com>
2026-04-27 21:44:26 -0500

    Setting up tradingJournal project on Avalonia UI.

---
+++ c_sharp/TradingJournal/TradingJournal/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TradingJournal.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
